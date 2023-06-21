9f3b1cf4faa4032d893a3d3e9215098c56522fec
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-06-21 14:39:02 -0300

    fix peer dependency range of pre-release (0.*.*) library

--- example/pages/api/get-treatment.js
+++ example/pages/api/get-treatment.js
@@ -26,7 +26,7 @@ export default async function handler(req, event) {
     storage: PluggableStorage({
       wrapper: EdgeConfigWrapper({
         // The Edge Config item where Split stores feature flag definitions, specified in the Split integration step
-        edgeConfigKey: process.env.EDGE_CONFIG_ITEM_KEY,
+        edgeConfigKey: process.env.SPLIT_EDGE_CONFIG_ITEM_KEY,
         // The Edge Config client. In this case, we are passing the default client
         // that reads from the Edge Config stored in process.env.EDGE_CONFIG
         edgeConfig: EdgeConfigClient
